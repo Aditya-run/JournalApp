c017e2d55cb2790c65e2451834c25c9add591965
Aditya-run <adityast473@gmail.com>
2026-02-20 07:48:24 +0530

    Initial commit - Journal Spring Boot Project

---
+++ journalApp/src/main/java/com/example/journalApp/Controller/JournalEntryController2.java
@@ -0,0 +1,43 @@
+package com.example.journalApp.Controller;
+
+import com.example.journalApp.entity.JournalEntry;
+import com.example.journalApp.service.JournalEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController2 {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return journalEntryService.getAll();
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntryService.saveEntry(myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return null;
+    }
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return null;
+    }
+    @PutMapping ("id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
+        return null;
+    }
+}
